0c03a4b4e5ac2f007b1c84cea94b7deb5d678106
Aymeric PINEL <aymeric.pinel@outlook.fr>
2021-03-18 23:46:26 +0100

    add menu closes #2, closes #3

--- app/src/main/java/com/m2dl/cracotte/cracottechallenge/menu/MenuActivity.java
+++ app/src/main/java/com/m2dl/cracotte/cracottechallenge/menu/MenuActivity.java
@@ -1,14 +1,34 @@
-package com.m2dl.cracotte.cracottechallenge;
+package com.m2dl.cracotte.cracottechallenge.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.m2dl.cracotte.cracottechallenge.R;
 
 public class MenuActivity extends AppCompatActivity {
+    private Button playButton;
+    private Button leaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+        initComponents();
+    }
+
+    private void initComponents() {
+        initPlayButton();
+        initLeaveButton();
+    }
+
+    private void initPlayButton() {
+        playButton = findViewById(R.id.menu_button_lancerJeu);
+    }
+
+    private void initLeaveButton() {
+        leaveButton = findViewById(R.id.menu_button_quitterJeu);
+        leaveButton.setOnClickListener(v -> finish());
     }
 }
