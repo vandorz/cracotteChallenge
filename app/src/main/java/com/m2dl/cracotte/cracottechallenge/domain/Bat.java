0da9454cb28b5858a6d73a93ad6cd05dde545877
Aymeric PINEL <aymeric.pinel@outlook.fr>
2021-03-19 09:24:55 +0100

    basic domain structure

---
+++ app/src/main/java/com/m2dl/cracotte/cracottechallenge/domain/Bat.java
@@ -0,0 +1,11 @@
+package com.m2dl.cracotte.cracottechallenge.domain;
+
+import android.graphics.Bitmap;
+
+public class Bat extends Sprite {
+
+    public Bat(Bitmap image) {
+        super();
+        this.setImage(image);
+    }
+}
