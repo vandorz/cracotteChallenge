d60495578002b134dc7dddc27c6e1f8d5cb85350
Clément GUICHARD <clement.guichard0@gmail.com>
2021-03-19 09:52:31 +0100

    add empy Game

---
+++ app/src/main/java/com/m2dl/cracotte/cracottechallenge/game/GameThread.java
@@ -0,0 +1,49 @@
+package com.m2dl.cracotte.cracottechallenge.game;
+
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+    private final SurfaceHolder surfaceHolder;
+    private final GameView gameView;
+    private final Handler threadHandler;
+
+    private boolean running;
+
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+        this.threadHandler = new Handler();
+    }
+
+    @Override
+    public void run() {
+        if (running) {
+            Canvas canvas = null;
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    gameView.update();
+                    gameView.draw(canvas);
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                threadHandler.postDelayed(this, 1000/60);
+            }
+        }
+    }
+
+    public void setRunning(boolean isRunning) {
+        running = isRunning;
+    }
+}
